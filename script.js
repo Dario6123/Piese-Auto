18310c509a180fb1bfff09769695c502355aed66
Dario6123 <you@example.com>
2025-10-25 18:48:09 +0300

    Actualizare proiect fianl

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ modeBtn.addEventListener("click", () => {
     modeBtn.textContent = "🌙 Night Mode";
   }
 });
+
+// Buton scroll sus
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
